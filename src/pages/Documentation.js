6cac4359098de94ed0b74f543c1f976918ca85c5
Saksham Gupta <saksham1319@gmail.com>
2021-04-08 20:45:00 +0530

    doc

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -9,23 +9,7 @@ export default class Documentation extends Component {
                 flexDirection: "column",
                 justifyContent: "center"
               }}>
-                 <div
-                    class="col"
-                    style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    margin:"40px"
-                    }}
-                >
-                    <h3>Measure Problem </h3>
-                    <h5>
-                    <a href="https://drunkenlegend.github.io/measure-documentation/">
-                        Go to Documentation
-                    </a>
-                    </h5>
-                </div>
+                
                 <div
                     class="col"
                     style={{
@@ -38,7 +22,7 @@ export default class Documentation extends Component {
                 >
                     <h3>Contour Problem </h3>
                     <h5>
-                    <a href="https://drunkenlegend.github.io/contour-documentation/">
+                    <a href="https://drunkenlegend.github.io/segmented-least-documentation/">
                         Go to Documentation
                     </a>
                     </h5>
